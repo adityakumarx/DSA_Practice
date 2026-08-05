efbf3092548793ce64d135233f0c57b7f945dbd4
adityakumarx <adityakumar55451@gmail.com>
2026-08-05 19:27:22 +0530

    Time: 0 ms (100%), Space: 9.5 MB (99.89%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,6 +1,3 @@
-//Valid Palindrome
-//learned: use of isalnum(checks alphanumeric values 'a' to 'z','A' to 'Z' and 0 to 9;
-//learned: use of tolower(changes case of char in a string)
 class Solution {
 public:
     bool isPalindrome(string_view s) {
