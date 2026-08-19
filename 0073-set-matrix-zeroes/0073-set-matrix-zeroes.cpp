a99684a3388121371800abe5483bc6d5f1b81751
adityakumarx <adityakumar55451@gmail.com>
2026-08-19 15:50:21 +0530

    Time: 0 ms (100%), Space: 21 MB (40.13%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,23 +4,31 @@ public:
 {
 	if (arr.empty())
 		return;
+
 	size_t a{ arr.size() };
 	size_t b{ arr[0].size() };
+
 	bool rowFlag{ false };
 	bool columnFlag{ false };
-	bool primeFlag{ false };
+
 	for (size_t i{ 0 };i < a;++i)
 	{
 		if (arr[i][0] == 0)
+        {
 			rowFlag = true;
+            break;
+        }
 	}
+
 	for (size_t j{ 0 };j < b;++j)
 	{
 		if (arr[0][j] == 0)
+        {
 			columnFlag = true;
+            break;
+        }
 	}
-	if (arr[0][0] == 0)
-		primeFlag = 0;
+
 	for (size_t i{ 1 };i < a;++i)
 	{
 		for (size_t j{ 1 };j < b;++j)
@@ -36,11 +44,7 @@ public:
 	{
 		for (size_t j{ 1 };j < b;++j)
 		{
-			if (arr[i][0] == 0)
-			{
-				arr[i][j] = 0;
-			}
-			if (arr[0][j] == 0)
+			if (arr[i][0] == 0 || arr[0][j] == 0)
 			{
 				arr[i][j] = 0;
 			}
@@ -58,17 +62,6 @@ public:
 		for (size_t j{ 0 };j < b;++j)
 		{
 			arr[0][j] = 0;
-		}
-	}
-	if (primeFlag)
-	{
-		for (size_t j{ 0 };j < b;++j)
-		{
-			arr[0][j] = 0;
-		}
-		for (size_t i{ 0 };i < a;++i)
-		{
-			arr[i][0] = 0;
 		}
 	}
 }
