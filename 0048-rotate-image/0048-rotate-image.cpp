c28d5cb3b66f6c827f999042c8f56b25ca99f642
adityakumarx <adityakumar55451@gmail.com>
2026-08-19 20:35:01 +0530

    Time: 0 ms (100%), Space: 10.2 MB (70.72%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,15 +4,19 @@ public:
 {
 	if (arr.empty())
 		return;
-	size_t n{ arr.size() };
-	for (size_t i{0};i < size(arr) / 2;++i)
+	[[maybe_unused]] size_t n{ arr.size() };
+	for (size_t i{ 0 };i < size(arr);++i)
 	{
-		for (size_t j{ 0 };j+2*i < size(arr)-1;++j)
+		for (size_t j{ i };j < size(arr);++j)
 		{
-			swap(arr[i][j+i], arr[j+i][n - 1-i]);
-			swap(arr[n-1-j-i][i], arr[i][j+i]);
-			swap(arr[n-1-i][n-1-j-i], arr[n-1-j-i][i]);
+			int temp = arr[i][j];
+			arr[i][j] = arr[j][i];
+			arr[j][i] = temp;
 		}
+	}
+	for (size_t i{ 0 };i < size(arr);++i)
+	{
+		reverse(arr[i].begin(), arr[i].end());
 	}
 }
 };
