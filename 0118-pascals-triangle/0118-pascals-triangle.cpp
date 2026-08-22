5cabd314980629b38b99e2322f43de1c52dacb19
adityakumarx <adityakumar55451@gmail.com>
2026-08-23 00:04:21 +0530

    Time: 0 ms (100%), Space: 9.6 MB (62.79%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -5,7 +5,7 @@ public:
 	vector<vector<int>> traingle;
 	for (size_t i{ 0 };i < target;++i)
 	{
-		vector<int> row(i + 1, 1);
+		vector<int> row(i + 1, 1);//temporary row to insert in main traingle
 		for (size_t j = 1;j < i;++j)
 		{
 			row[j] = traingle[i - 1][j - 1] + traingle[i - 1][j];
