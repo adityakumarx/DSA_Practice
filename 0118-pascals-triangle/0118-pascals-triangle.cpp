e4a6334385b5e4d8541d25f5a853d2d2533da30e
adityakumarx <adityakumar55451@gmail.com>
2026-08-30 14:58:58 +0530

    Time: 3 ms (7.23%), Space: 9.7 MB (62.42%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int target)
-{
-	vector<vector<int>> traingle;
-	for (size_t i{ 0 };i < target;++i)
-	{
-		vector<int> row(i + 1, 1);//temporary row to insert in main traingle
-		for (size_t j = 1;j < i;++j)
-		{
-			row[j] = traingle[i - 1][j - 1] + traingle[i - 1][j];
-		}
-		traingle.push_back(row);
-	}
-	return traingle;
-}
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> traingle;
+        for(int n{0};n<numRows;++n)
+        {
+            vector<int> row;
+            row.push_back(1);
+            for(int r{1};r<n+1;++r)
+            {
+                row.push_back((row[static_cast<size_t>(r)-1]*(n-r+1))/r);
+            }
+            traingle.push_back(row);
+        }
+        return traingle;
+    }
 };
